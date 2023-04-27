c0a630b364aa8a1e5b31e87ba661f791f16061c8
Dorka <dorka.kovacs2006@gmail.com>
2023-04-27 19:38:08 +0200

    04.27 - 1
    Co-authored-by: droppedcode <droppedcode@users.noreply.github.com>
    Co-authored-by: zsanlengyel <zsanlengyel@users.noreply.github.com>
    Co-authored-by: Felkifli <Felkifli@users.noreply.github.com>

--- src/entities/field.ts
+++ src/entities/field.ts
@@ -9,6 +9,21 @@ export class Field extends Entity {
   public x: number = 0;
   public y: number = 0;
 
+  // local oxygen
+  
+  /**
+   * Create a Field instance on the given coordinates.
+   * 
+   * @argument x X coordinate of the field.
+   * @argument y Y coordinate of the field.
+   */
+  constructor(x: number, y: number) {
+    super();
+
+    this.x = x;
+    this.y = y;
+  }
+
   public override step(context: SimulationContext): void {
     for (const entity of [...this.entities]) {
       entity.step({
